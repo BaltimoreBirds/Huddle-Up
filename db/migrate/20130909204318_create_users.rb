41f39353728e462bd6623b4a6113b103f99d959a
Michael Swanson <mikeswanson12@msn.com>
2013-09-09 16:57:31 -0400

    spec_helper

---
+++ db/migrate/20130909204318_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :location
+      t.integer :skill
+
+      t.timestamps
+    end
+  end
+end
