eac249ef40dc40c5521f6957762443bb19fe92a7
Michael Swanson <mikeswanson12@msn.com>
2013-09-09 16:30:47 -0400

    initial commit, switch to postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BreakableToy2::Application.config.secret_key_base = '8585fa9b111b3be2cfd4cf0ce20a3d6377677671e02e5fed314d66affd6088598b2fb7551f85b0e1bd3b9e53a1bf4ee065e2904ee6085f05d40c30568892c7c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BreakableToy2::Application.config.session_store :cookie_store, key: '_breakable_toy2_session'
