3f521df252ea547e5a4d55c6ee9cb63a8d0cac64
Michael Swanson <mikeswanson12@msn.com>
2013-09-18 20:02:25 -0400

    first feature test failing

--- spec/features/new_user_signs_up_spec.rb
+++ spec/features/new_user_signs_up_spec.rb
@@ -15,14 +15,18 @@ So that I may find Huddles
 # -thank you for signing up page is displayed, that also
 #   makes note of the verification email
 
-  it 'signs up with valid information' do
+  it 'user registers with valid information' do
     FactoryGirl.create(:user)
     prev_count = User.count
 
     visit '/'
+    fill_in 'Username', with: "Jahsprout"
     fill_in 'Email', with: 'mjakejake@msn.com'
     fill_in 'Password', with: 'hopefully'
+    fill_in 'city', with: 'SomePlace'
+    choose 'Advanced'
     click_on('Sign in')
+    # binding.pry
     expect(User.count).to eql(prev_count + 1)
   end
 
