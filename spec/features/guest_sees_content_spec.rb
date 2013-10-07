11b76d13763bae762dc88d1b21091a3c89f18bfa
Michael Swanson <mikeswanson12@msn.com>
2013-10-07 08:52:14 -0400

    new feature specs added

---
+++ spec/features/guest_sees_content_spec.rb
@@ -0,0 +1,17 @@
+require'spec_helper'
+
+feature'user sees and interacts with content', %Q{
+  As a guest
+I want to be able to view huddles and content
+So that I may be decide whether to register
+
+
+} do
+
+# Acceptance Criteria
+# *Unregistered user sees guest button on home page
+# *User is assigned random temporary guest attributes
+# *User can view huddles/locations
+# *if user tries to join huddle, user gets message, asking them to register,
+#    which redirects to registration pageh5
+end
